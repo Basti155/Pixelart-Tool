a8fb17822851aaaa3707edf0f287f93702c2d8ca
Unknown <basti597@gmail.com>
2017-11-15 20:19:07 +0000

    Add Eraser

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -26,19 +26,22 @@ import java.util.ResourceBundle;
  */
 public class Controller implements Initializable
 {
-    private GraphicsContext layer,layerGrid, layerHover;
+    private GraphicsContext layer,layerGrid, layerHover, layerBackground;
     private WritableImage wim;
     private Color color;
 
     private List<Canvas> canvasDict = new ArrayList<>();
 
-    private int brushSize, imagesSize, brushScale;
+    private int brushSize;
+    private int brushScale;
     private double scale, x, y;
+
+    private boolean erase;
 
 
 
     @FXML
-    private Canvas canvas, canvasGrid ,canvasHover;
+    private Canvas canvas, canvasGrid ,canvasHover, canvasBackground;
 
     @FXML
     private GridPane gp;
@@ -59,6 +62,7 @@ public class Controller implements Initializable
         scale = 1.0;
         brushScale = 1;
 
+        canvasDict.add(canvasBackground);
         canvasDict.add(canvas);
         canvasDict.add(canvasGrid);
         canvasDict.add(canvasHover);
@@ -69,43 +73,12 @@ public class Controller implements Initializable
         setEvents();
     }
 
-    private void newCanvas()
-    {
-        imagesSize = (int)canvas.getHeight();
-        brushSize = imagesSize / 20;
-
-        color = Color.BLACK;
-        colorPicker.setValue(color);
-
-        layer = canvas.getGraphicsContext2D();
-        layerGrid = canvasGrid.getGraphicsContext2D();
-        layerHover = canvasHover.getGraphicsContext2D();
-
-        wim = new WritableImage(imagesSize, imagesSize);
-
-        canvasHover.toFront();
-
-        drawGrid();
-    }
-
     private void setEvents()
     {
         brush.getSelectionModel().selectedIndexProperty().addListener(
                 (observable, oldValue, newValue) -> brushScale = (int)newValue  + 1);
 
-                canvasHover.addEventHandler(MouseEvent.MOUSE_MOVED, e -> {
-                    layerHover.setFill(Color.WHITE);
-                    layerHover.clearRect(0, 0, canvasHover.getWidth(), canvasHover.getHeight());
-
-                    x = ((e.getX() - (brushSize / 2) * brushScale) / brushSize);
-                    x = ((double) Math.round(x * 1) / 1) * brushSize;
-
-                    y = ((e.getY() - (brushSize / 2) * brushScale) / brushSize);
-                    y = ((double) Math.round(y * 1) / 1) * brushSize;
-
-                    layerHover.setFill(color);
-                    layerHover.fillRect(x, y, brushSize * brushScale, brushSize * brushScale);
-                });
+                canvasHover.addEventHandler(MouseEvent.MOUSE_MOVED, e -> drawBrush(e.getX(), e.getY()));
 
         canvasHover.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
             layer.setFill(color);
@@ -113,27 +86,12 @@ public class Controller implements Initializable
         });
 
         canvasHover.addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> {
-            layerHover.setFill(Color.WHITE);
-            layerHover.clearRect(0, 0, canvasHover.getWidth(), canvasHover.getHeight());
+            drawBrush(e.getX(), e.getY());
 
-            x = ((e.getX() -(brushSize/2)*brushScale) / brushSize);
-            x = ((double)Math.round(x * 1) / 1) * brushSize;
-
-            y = ((e.getY() -(brushSize/2)*brushScale) / brushSize);
-            y = ((double)Math.round(y * 1) / 1) * brushSize;
-
-            layerHover.setFill(color);
-            layerHover.fillRect(x, y, brushSize * brushScale, brushSize * brushScale);
-
-
-            x = ((e.getX() -(brushSize/2)*brushScale) / brushSize);
-            x = ((double)Math.round(x * 1) / 1) * brushSize;
-
-            y = ((e.getY() -(brushSize/2)*brushScale) / brushSize);
-            y = ((double)Math.round(y * 1) / 1) * brushSize;
-
-            layer.setFill(color);
-            layer.fillRect(x, y, brushSize * brushScale, brushSize * brushScale);
+            if(!erase)
+                draw(e.getX(), e.getY());
+            else
+                clear(e.getX(), e.getY());
         });
 
         canvasHover.addEventHandler(ScrollEvent.SCROLL, e -> {
@@ -156,6 +114,73 @@ public class Controller implements Initializable
         });
     }
 
+    private void newCanvas()
+    {
+        int imagesSize = (int) canvas.getHeight();
+        brushSize = imagesSize / 20;
+
+        color = Color.BLACK;
+        colorPicker.setValue(color);
+
+        layerBackground = canvasBackground.getGraphicsContext2D();
+        layer = canvas.getGraphicsContext2D();
+        layerGrid = canvasGrid.getGraphicsContext2D();
+        layerHover = canvasHover.getGraphicsContext2D();
+
+        wim = new WritableImage(imagesSize, imagesSize);
+
+        canvasHover.toFront();
+
+        drawGrid();
+        drawBackground();
+    }
+
+    private void drawBackground()
+    {
+        double w, h;
+        w = canvasBackground.getWidth() / 20;
+        h = canvasBackground.getHeight() / 20;
+
+        for(int x = 0; x < h; x++)
+        {
+            for (int y = 0; y < w; y++)
+            {
+                if ((y % 2) == 0)
+                    layerBackground.setFill(Color.GREY);
+                else
+                    layerBackground.setFill(Color.LIGHTGREY);
+
+                layerBackground.fillRect(x * brushSize, y * brushSize, brushSize, brushSize);
+            }
+        }
+
+    }
+
+    private void drawGrid()
+    {
+        double w, h;
+        w = canvasGrid.getWidth() / 20;
+        h = canvasGrid.getHeight() / 20;
+
+        layerGrid.setFill(Color.LIGHTGRAY);
+
+        double x1, y1;
+
+        for(int i = 0; i < w; i++)
+        {
+            x1 = w * i;
+
+            layerGrid.strokeLine(x1, 0, x1, canvasGrid.getHeight());
+        }
+
+        for(int i = 0; i < w; i++)
+        {
+            y1 = h * i;
+
+            layerGrid.strokeLine(0, y1, canvasGrid.getWidth(), y1);
+        }
+    }
+
     private void setScale(double scale)
     {
         for (Canvas c : canvasDict )
@@ -170,6 +195,54 @@ public class Controller implements Initializable
 
         lblScale.setText("Scale: " + scale);
     }
+
+
+    private void drawBrush(double x, double y)
+    {
+        layerHover.setFill(Color.WHITE);
+        layerHover.clearRect(0, 0, canvasHover.getWidth(), canvasHover.getHeight());
+
+        this.x = ((x - (brushSize / 2) * brushScale) / brushSize);
+        this.x = ((double) Math.round(this.x * 1) / 1) * brushSize;
+
+        this.y = ((y - (brushSize / 2) * brushScale) / brushSize);
+        this.y = ((double) Math.round(this.y * 1) / 1) * brushSize;
+
+        layerHover.setFill(color);
+        layerHover.fillRect(this.x, this.y, brushSize * brushScale, brushSize * brushScale);
+
+
+        if(!color.equals(Color.BLACK))
+            layerHover.setStroke(Color.BLACK);
+        else
+            layerHover.setStroke(Color.WHITE);
+
+        layerHover.strokeRect(this.x, this.y, brushSize * brushScale, brushSize * brushScale);
+    }
+
+    private void draw(double x, double y)
+    {
+        this.x = ((x -(brushSize/2)*brushScale) / brushSize);
+        this.x = ((double)Math.round(this.x * 1) / 1) * brushSize;
+
+        this.y = ((y -(brushSize/2)*brushScale) / brushSize);
+        this.y = ((double)Math.round(this.y * 1) / 1) * brushSize;
+
+        layer.setFill(color);
+        layer.fillRect(this.x, this.y, brushSize * brushScale, brushSize * brushScale);
+    }
+
+    private void clear(double x, double y)
+    {
+        this.x = ((x -(brushSize/2)*brushScale) / brushSize);
+        this.x = ((double)Math.round(this.x * 1) / 1) * brushSize;
+
+        this.y = ((y -(brushSize/2)*brushScale) / brushSize);
+        this.y = ((double)Math.round(this.y * 1) / 1) * brushSize;
+
+        layer.clearRect(this.x, this.y, brushSize * brushScale, brushSize * brushScale);
+    }
+
 
     @FXML
     private void saveImage()
@@ -214,35 +287,16 @@ public class Controller implements Initializable
     }
 
     @FXML
+    private void clearBool()
+    {
+        erase = !erase;
+    }
+
+
+    @FXML
     private void exit()
     {
         // TODO Check if saved
         System.exit(0);
     }
-
-    private void drawGrid()
-    {
-        double w, h;
-        w = canvasGrid.getWidth() / 20;
-        h = canvasGrid.getHeight() / 20;
-
-        layerGrid.setFill(Color.LIGHTGRAY);
-
-        double x1, y1 = 0;
-
-        for(int i = 0; i < w; i++)
-        {
-            x1 = w * i;
-
-            layerGrid.strokeLine(x1, 0, x1, canvasGrid.getHeight());
-        }
-
-        for(int i = 0; i < w; i++)
-        {
-            y1 = h * i;
-
-            layerGrid.strokeLine(0, y1, canvasGrid.getWidth(), y1);
-        }
-    }
-
 }
